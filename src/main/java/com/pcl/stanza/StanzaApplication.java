78f8720acc40a37477d827ae1cae280bd94e904f
Rod Johnson <johnsonroda@gmail.com>
2018-02-25 21:00:21 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/pcl/stanza/StanzaApplication.java
@@ -0,0 +1,12 @@
+package com.pcl.stanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StanzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StanzaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pcl/stanza/StanzaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pcl.stanza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StanzaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
